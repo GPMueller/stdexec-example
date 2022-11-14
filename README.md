61e077ac7fe4fe1db2516559d4106d8bd50d99bd
G. P. Mueller <gpmueller@outlook.com>
2022-11-14 11:25:03 +0100

    First draft for switching CPU and GPU schedulers

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # stdexec-example
+
 Examples of using the stdexec library for parallel computation
+
+## Build
+
+Use the standard CPU scheduler
+```
+cmake . -B build -DCMAKE_CXX_COMPILER=clang++
+cmake --build build
+```
+
+Use the CUDA GPU scheduler
+```
+cmake . -B build -DCMAKE_CXX_COMPILER=clang++ -DCUDA_HOST_COMPILER=clang++ -DCMAKE_CUDA_COMPILER=clang++ -DSTDEXEC_EXAMPLE_USE_CUDA=ON
+cmake --build build
+```
--- source/main.cpp
+++ source/main.cpp
@@ -4,13 +4,47 @@
 #include <stdexec/execution.hpp>
 #include <vector>
 
+#ifdef STDEXEC_EXAMPLE_USE_CUDA
+#include <nvexec/stream_context.cuh>
+class compute_resource
+{
+  public:
+    compute_resource() :
+        stream_context()
+    { }
+
+    stdexec::scheduler auto get_scheduler()
+    {
+        return stream_context.get_scheduler();
+    }
+
+  private:
+    nvexec::stream_context stream_context;
+};
+#else
+class compute_resource
+{
+  public:
+    compute_resource() :
+        pool(exec::static_thread_pool(8))
+    { }
+
+    stdexec::scheduler auto get_scheduler()
+    {
+        return pool.get_scheduler();
+    }
+
+  private:
+    exec::static_thread_pool pool;
+};
+#endif
+
 int main()
 {
     const int N = 24; // 10'000;
     std::vector<double> values(N);
-
-    exec::static_thread_pool pool(8);
-    auto sched = pool.get_scheduler();
+    compute_resource resource;
+    stdexec::scheduler auto sched = resource.get_scheduler();
 
     auto bulk_work = stdexec::bulk(N,
         [=](std::size_t i, std::vector<double>& values) {
@@ -18,8 +52,9 @@ int main()
             // std::cout << i << "\n";
         });
 
-    auto work = stdexec::transfer_just(sched, std::move(values))
+    stdexec::sender auto work = stdexec::transfer_just(sched, std::move(values))
         | bulk_work;
+
     stdexec::this_thread::sync_wait(work);
 
     // std::cout << std::endl;
